ecdcf2827cb39de7bf397921b25857ab351455d4
ankitkataria <ankitkataria28@gmail.com>
2018-10-13 16:59:36 +0530

    Implements web3 to interact with contract

--- node-client/contracts/Files.sol
+++ node-client/contracts/Files.sol
@@ -18,38 +18,38 @@ contract Files {
 	mapping (address => PrivateDoc[]) public privateShares;
 
 	// mapping from an id to describe a file to its meta deta
-	mapping (address => PublicDoc) public publicShares;
+	mapping (uint => PublicDoc) public publicShares;
 
 	function _addPrivate(string _url, address _journalist) private {
 		// the url coming has to be already encrypted
-		PrivateDoc doc = PrivateDoc({
+		privateShares[_journalist].push(PrivateDoc({
 			url: _url,
 			uploader: msg.sender
-		});
-
-		privateShares[_journalist].push(doc);
+		}));
 	}
 
 	function _addPublic(string _url) private {
-		PublicDoc doc = PublicDoc({
+		publicShares[uint(keccak256(abi.encodePacked(_url)))] = PublicDoc({
 			url: _url,
 			upvotes: 0,
 			downvotes: 0,
 			uploader: msg.sender
 		});
-
-		publicShares[uint(keccak256(_url))] = doc;
 	}
 
-	function _vote(uint _id, uint _change) private {
-		publicShares[_id] += _change;
-	}
-
-	function addDocument(string _url, bool _isPublic, address _shareTo = address(0)) public {
-		if(isPublic) 
-			_addPublic(_url);
+	function vote(uint _id, int _change) public {
+		if(_change == 1)
+			publicShares[_id].upvotes += 1;
 		else 
-			_addPrivate(_url, _shareTo);
+			publicShares[_id].downvotes -=1;
+	}
+
+	function addPublicDocument(string _url) public {
+		_addPublic(_url);
+	}
+
+	function addDocument(string _url, address _shareTo) public {
+		_addPrivate(_url, _shareTo);
 	}
 }
 
